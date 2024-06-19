45456690ac50cd3371ee698a04a67e687e348406
bhanupratapsingh21 <bhanupss137@gmail.com>
2024-06-19 18:41:15 +0530

    done with socket io server

--- web-Socketsv-1/index.js
+++ web-Socketsv-1/index.js
@@ -1,22 +1,36 @@
-import http, { Server } from "http"
-import path from "path"
-import express from "express"
-import { Server } from "socket.io"
-
+import http from "http";
+import path from "path";
+import express from "express";
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
-io.on("connection", (socket) => {
-    console.log("new user connected" ,socket.id);
-});
-
+// Serve static files from the "public" directory
 app.use(express.static(path.resolve("./public")));
 
+// Serve the index.html file for the root URL
 app.get("/", (req, res) => {
-    return res.sendFile("./public/index.html")
-})
+    return res.sendFile(path.resolve("./public/index.html"));
+});
 
-server.listen(9000, () => console.log("Server Started At", 9000))
+// Handle socket connections
+io.on("connection", (socket) => {
+    console.log("A user connected");
 
-// socket 
+    // Handle incoming user messages
+    socket.on("user-message", (message) => {
+        console.log("Received message:", message); // Log the received message
+        // Broadcast the message to all connected clients
+        io.emit("user-message", message);
+    });
+
+    // Handle user disconnect
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
+
+// Start the server
+server.listen(9000, () => console.log("Server started at port 9000"));
